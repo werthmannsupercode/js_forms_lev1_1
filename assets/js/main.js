d140a81dc9589714f4c57742e15d3f1b67c3257c
werthmannsupercode <werthmannsupercode@gmail.com>
2022-04-19 14:59:44 +0200

    first try

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+let number = document.getElementById('number');
+let ergebnis = document.getElementById('ergebnis');
+
+function verdopplung() {
+    let zweiMal = number.value * 2;
+    ergebnis.innerHTML = zweiMal;
+}
